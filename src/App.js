dd07db68db8068b0d71c058af4f365cfde276255
Emre AY <ay.vet.hek@gmail.com>
2023-06-04 01:06:56 +0300

    css tutorial

--- src/App.js
+++ src/App.js
@@ -3,32 +3,12 @@ import './App.css';
 
 function App() {
 
-  const test = 34;
-  const isAuth = true;
 
   return (
 
-    <div className="App">
+    <div className='container'>
+      <h4 >App Component</h4>
 
-
-      {/* herşey tek div altında yapılır
-    class değil clasName kullanılır. */}
-
-      {/* <h1>Merhaba React</h1>
-     <h1 className ="naber">  class değil className olmalı </h1>
-     <label htmlFor=""> </label> */}
-      {/* JS ifadeleri kullanacağımız zaman süslü parantez içerisine alıyoruz.  */}
-
-      {/* <h4> {1+1} </h4>
-     <h4> {"murat".toUpperCase()} </h4>
-    <h4>{test} </h4> 
-<div>
-  {
-    isAuth ? <p>Kullanıcı Kayıtlı</p>
-    : null
-  
-  }
-</div> */}
 
     </div>
   );
